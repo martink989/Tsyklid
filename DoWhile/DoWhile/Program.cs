7ce2126487706bb21e0cb8510df58f20a5e87714
Opilane TTHK <opilane@tthk.ee>
2025-10-13 09:12:14 +0300

    do while joonistab ruudu

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+string kasutajaNimi = "";
+do
+{
+    Console.WriteLine("Palun sisesta oma kasutajanimi");
+    kasutajaNimi = Console.ReadLine();
+}
+while (kasutajaNimi != "user1");
+if (kasutajaNimi == "user1")
+{
+    int ruuduSuurus = 0;
+
+    do
+    {
+        Console.WriteLine("Kui suurt ruutu tahad?");
+        ruuduSuurus = int.Parse(Console.ReadLine());
+    } while (ruuduSuurus < 0 && ruuduSuurus > 20);
+    char reakujund = '*';
+    string üksRida = "";
+    int tsükliMuutuja = ruuduSuurus;
+    do
+    {
+        üksRida += "_"+reakujund;
+        tsükliMuutuja -= 1;
+    } while (tsükliMuutuja != 0);
+    tsükliMuutuja = ruuduSuurus;
+    do
+    {
+        Console.WriteLine(üksRida);
+        tsükliMuutuja -= 1;
+
+    } while (tsükliMuutuja != 0);
+}
+
